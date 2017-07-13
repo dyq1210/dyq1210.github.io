d5e4ce0f3a28f361300aec2963a9011b53ee4646
dyq1210 <3024453121@qq.com>
2017-07-13 19:14:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let countDown = new Date('july 14, 2017 00:00:00').getTime(),
+    x = setInterval(function () {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24)),
+        document.getElementById('hours').innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        document.getElementById('minutes').innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+        document.getElementById('seconds').innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, 1000)
