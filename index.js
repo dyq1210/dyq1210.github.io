000970f938ba434f3d6c3803601fc23fed909f54
dyq1210 <3024453121@qq.com>
2017-07-23 17:09:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ let countDown = new Date('Sep 30, 2017 00:00:00').getTime(),
         document.getElementById('hours').innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
         document.getElementById('minutes').innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
         document.getElementById('seconds').innerHTML = Math.floor((distance % (1000 * 60)) / 1000)
-      //do something later when date is reached
+     if (distance < 0)  location.href='happy.html'; 
+        
+        
+        //do something later when date is reached
       //if (distance < 0) {
       //  clearInterval(x);
       //  'IT'S MY BIRTHDAY!;
