e03715ce6e8cd48dea73d22d8260e3f8898f826d
dyq1210 <3024453121@qq.com>
2017-08-23 17:13:09 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,96 +1,201 @@
-"use strict";
-
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
-var w = canvas.width = window.innerWidth;
-var h = canvas.height = window.innerHeight;
-ctx.lineCap = "round";
-ctx.lineWidth = 5;
-ctx.shadowBlur = 15;
-
-function Segment(x, y, angle, angleSize, radius) {
-  this.x = x;
-  this.y = y;
-  this.angle = angle;
-  this.angleSize = angleSize;
-  this.r = radius;
-  this.x1 = 0;
-  this.y1 = 0;
-  this.x2 = 0;
-  this.y2 = 0;
-  this.color = "hsl(" + angle * 180 / Math.PI + ", 80%, 60%)";
-}
-
-Segment.prototype.draw = function () {
-  ctx.save();
-  ctx.fillStyle = this.color;
-  ctx.strokeStyle = this.color;
-  ctx.shadowColor = this.color;
-
-  // Put origin at the center of the flower
-  ctx.translate(this.x, this.y);
-  // Now we can rotate around origin
-  ctx.rotate(this.angle);
-
-  // That way we don't need to think when
-  // drawing each segment. Just draw because
-  // it is already rotated for us
-  ctx.beginPath();
-  var x = Math.cos(this.angleSize / 2) * this.r;
-  var y = Math.sin(this.angleSize / 2) * this.r;
-  ctx.moveTo(x, -y);
-  ctx.bezierCurveTo(this.x1, this.y1, this.x2, this.y2, x, y);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(this.x1, this.y1, 5, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.beginPath();
-  ctx.arc(this.x2, this.y2, 5, 0, Math.PI * 2);
-  ctx.fill();
-
-  ctx.restore();
-
-  var factor = 1.4;
-  this.x1 = Math.cos(tick) * this.r * factor;
-  this.y1 = Math.sin(tick) * this.r * factor;
-  this.x2 = Math.cos(-tick) * this.r * factor;
-  this.y2 = Math.sin(-tick) * this.r * factor;
+/*=============================================================================*/	
+/* Smooth Trail
+/* https://codepen.io/jackrugile/pen/ohFbx
+/*=============================================================================*/
+var smoothTrail = function(c, cw, ch){
+  
+/*=============================================================================*/	
+/* Initialize
+/*=============================================================================*/
+  this.init = function(){
+    this.loop();
+  };		
+  
+  /*=============================================================================*/	
+  /* Variables
+/*=============================================================================*/
+  var _this = this;
+  this.c = c;
+  this.ctx = c.getContext('2d');
+  this.cw = cw;
+  this.ch = ch;
+  this.mx = 0;
+  this.my = 0;
+  
+  //trail
+  this.trail = [];
+  this.maxTrail = 200;
+  this.mouseDown = false;
+  
+  this.ctx.lineWidth = .1;
+  this.ctx.lineJoin = 'round';
+  
+  this.radius = 1;
+  this.speed = 0.4;
+  this.angle = 0;
+  this.arcx = 0;
+  this.arcy = 0;
+  this.growRadius = true;
+  this.seconds = 0;
+  this.milliseconds = 0;
+  
+  /*=============================================================================*/	
+  /* Utility Functions
+/*=============================================================================*/				
+  this.rand = function(rMi, rMa){return ~~((Math.random()*(rMa-rMi+1))+rMi);};
+  this.hitTest = function(x1, y1, w1, h1, x2, y2, w2, h2){return !(x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1);};
+  
+  /*=============================================================================*/	
+  /* Create Point
+/*=============================================================================*/
+  this.createPoint = function(x, y){					
+    this.trail.push({
+      x: x,
+      y: y						
+    });
+  };
+  
+  /*=============================================================================*/	
+  /* Update Trail
+/*=============================================================================*/
+  this.updateTrail = function(){					
+    
+    if(this.trail.length < this.maxTrail){
+      this.createPoint(this.arcx, this.arcy);
+    }					
+    
+    if(this.trail.length >= this.maxTrail){
+      this.trail.splice(0, 1);
+    }					
+  };
+  
+  /*=============================================================================*/	
+  /* Update Arc
+/*=============================================================================*/
+  this.updateArc = function(){
+    this.arcx = (this.cw/2) + Math.sin(this.angle) * this.radius;
+    this.arcy = (this.ch/2) + Math.cos(this.angle) * this.radius;					
+    var d = new Date();
+    this.seconds = d.getSeconds();
+    this.milliseconds = d.getMilliseconds();
+    this.angle += this.speed*(this.seconds+1+(this.milliseconds/1000));
+    
+    if(this.radius <= 1){
+      this.growRadius = true;
+    } 
+    if(this.radius >= 200){
+      this.growRadius = false;
+    }
+    
+    if(this.growRadius){
+      this.radius += 1;	
+    } else {
+      this.radius -= 1;	
+    }
+  };
+  
+  /*=============================================================================*/	
+  /* Render Trail
+/*=============================================================================*/
+  this.renderTrail = function(){
+    var i = this.trail.length;					
+    
+    this.ctx.beginPath();
+    while(i--){
+      var point = this.trail[i];
+      var nextPoint = (i == this.trail.length) ? this.trail[i+1] : this.trail[i];
+      
+      var c = (point.x + nextPoint.x) / 2;
+      var d = (point.y + nextPoint.y) / 2;						
+      this.ctx.quadraticCurveTo(Math.round(this.arcx), Math.round(this.arcy), c, d);
+      
+      
+      
+    };
+    this.ctx.strokeStyle = 'hsla('+this.rand(170,300)+', 100%, '+this.rand(50, 75)+'%, 1)';	
+    this.ctx.stroke();
+    this.ctx.closePath();
+    
+  };			
+ 
+  
+  /*=============================================================================*/	
+  /* Clear Canvas
+/*=============================================================================*/
+  this.clearCanvas = function(){
+    //this.ctx.globalCompositeOperation = 'source-over';
+    //this.ctx.clearRect(0,0,this.cw,this.ch);
+    
+    this.ctx.globalCompositeOperation = 'destination-out';
+    this.ctx.fillStyle = 'rgba(0,0,0,.1)';
+    this.ctx.fillRect(0,0,this.cw,this.ch);					
+    this.ctx.globalCompositeOperation = 'lighter';
+  };
+  
+  /*=============================================================================*/	
+  /* Animation Loop
+/*=============================================================================*/
+  this.loop = function(){
+    var loopIt = function(){
+      requestAnimationFrame(loopIt, _this.c);
+      _this.clearCanvas();
+      _this.updateArc();
+      _this.updateTrail();
+      _this.renderTrail();							
+    };
+    loopIt();					
+  };
+  
 };
 
-function Flower(x, y) {
-  this.x = x;
-  this.y = y;
-  this.numSegments = Math.round(Math.random() * 10 + 5);
+/*=============================================================================*/	
+/* Check Canvas Support
+/*=============================================================================*/
+var isCanvasSupported = function(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+};
 
-  // Make it fit on screen.
-  var radius = Math.min(w, h) / 3;
+/*=============================================================================*/	
+/* Setup requestAnimationFrame
+/*=============================================================================*/
+var setupRAF = function(){
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x){
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+  };
+  
+  if(!window.requestAnimationFrame){
+    window.requestAnimationFrame = function(callback, element){
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  };
+  
+  if (!window.cancelAnimationFrame){
+    window.cancelAnimationFrame = function(id){
+      clearTimeout(id);
+    };
+  };
+};			
 
-  // Divide a whole turn equally between
-  // all the segments.
-  var angleForEachSegment = Math.PI * 2 / this.numSegments;
-  this.segments = [];
-  for (var i = 0; i < this.numSegments; i++) {
-    var angle = i * angleForEachSegment;
-    var s = new Segment(this.x, this.y, angle, angleForEachSegment, radius);
-    this.segments.push(s);
+/*=============================================================================*/	
+/* Define Canvas and Initialize
+/*=============================================================================*/
+  if(isCanvasSupported){
+    var c = document.createElement('canvas');
+    c.width = 400;
+    c.height = 400;			
+    var cw = c.width;
+    var ch = c.height;	
+    document.body.appendChild(c);	
+    var cl = new smoothTrail(c, cw, ch);				
+    
+    setupRAF();
+    cl.init();
   }
-}
-
-Flower.prototype.draw = function () {
-  this.segments.forEach(function (s) {
-    return s.draw();
-  });
-};
-
-var f = new Flower(w / 2, h / 2);
-var tick = 0;
-
-function animation() {
-  requestAnimationFrame(animation);
-  ctx.clearRect(0, 0, w, h);
-  f.draw();
-  tick += 0.01;
-}
-
-animation();
