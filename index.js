4af848d639fe9e68ec5973c384044643eb844e35
dyq1210 <3024453121@qq.com>
2017-12-07 17:50:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let countDown = new Date('Dec 3, 2017 00:00:00').getTime(),
+let countDown = new Date('Dec 10, 2017 00:00:00').getTime(),
     x = setInterval(function () {
 
       let now = new Date().getTime(),
